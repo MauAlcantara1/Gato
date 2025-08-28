912b24cd930097d5da739072504e4616e5b6e8d3
MauAlcantara1 <japonxd1@hotmail.com>
2025-08-27 18:59:20 -0600

    Prueba

--- Assets/Codigos/gato.cs
+++ Assets/Codigos/gato.cs
@@ -18,6 +18,95 @@ public class gato : MonoBehaviour
         IniciaGato();
     }
 
+    private void EscribeValorMatrizGato(string btn, int t)
+    {
+        int valor = (t == 1) ? 1 : 2;
+        switch (btn)
+        {
+            case "c0": matrizGato[0, 0] = valor; break;
+            case "c1": matrizGato[0, 1] = valor; break;
+            case "c2": matrizGato[0, 2] = valor; break;
+            case "c3": matrizGato[1, 0] = valor; break;
+            case "c4": matrizGato[1, 1] = valor; break;
+            case "c5": matrizGato[1, 2] = valor; break;
+            case "c6": matrizGato[2, 0] = valor; break;
+            case "c7": matrizGato[2, 1] = valor; break;
+            case "c8": matrizGato[2, 2] = valor; break;
+        }
+    }
+    
+    private void VerificarGanador()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (matrizGato[i,0] != 0 && 
+                matrizGato[i,0] == matrizGato[i,1] && 
+                matrizGato[i,1] == matrizGato[i,2])
+            {
+                ganador = matrizGato[i,0];
+                MostrarGanador();
+                return;
+            }
+        }
+
+        for (int j = 0; j < 3; j++)
+        {
+            if (matrizGato[0,j] != 0 && 
+                matrizGato[0,j] == matrizGato[1,j] && 
+                matrizGato[1,j] == matrizGato[2,j])
+            {
+                ganador = matrizGato[0,j];
+                MostrarGanador();
+                return;
+            }
+        }
+
+        if (matrizGato[0,0] != 0 && 
+            matrizGato[0,0] == matrizGato[1,1] && 
+            matrizGato[1,1] == matrizGato[2,2])
+        {
+            ganador = matrizGato[0,0];
+            MostrarGanador();
+            return;
+        }
+
+        if (matrizGato[0,2] != 0 && 
+            matrizGato[0,2] == matrizGato[1,1] && 
+            matrizGato[1,1] == matrizGato[2,0])
+        {
+            ganador = matrizGato[0,2];
+            MostrarGanador();
+            return;
+        }
+
+        bool empate = true;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (matrizGato[i, j] == 0) empate = false;
+            }
+        }
+
+        if (empate)
+        {
+            txtJuego.text = "¡Empate!";
+        }
+    }
+
+    private void MostrarGanador()
+    {
+        if (ganador == 1)
+            txtJuego.text = "¡Ganó X!";
+        else if (ganador == 2)
+            txtJuego.text = "¡Ganó O!";
+
+        for (int i = 1; i <= 9; i++)
+        {
+            GameObject.Find("c" + i).GetComponent<Button>().interactable = false;
+        }
+    }
+
     public void AsignaTurno(Button btn)
     {
         if (ganador == 0)
@@ -25,10 +114,13 @@ public class gato : MonoBehaviour
             txtJuego.text = "Juego en curso";
 
             DibujaSimbolo(btn, turno);
+            EscribeValorMatrizGato(btn.name, turno);
+            VerificarGanador();
 
-            turno = (turno == 0) ? 1 : 0;
+            turno = (turno == 1) ? 2 : 1; // alternar entre 1 y 2
         }
     }
+
 
 
     private void DibujaSimbolo(Button btn, int turno)
